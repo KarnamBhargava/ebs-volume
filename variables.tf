f09a6e96e083eb1fd3d15a28295f0e7dfe982ec1
KarnamBhargava <karnam.b@cloudthat.com>
2025-04-23 17:08:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,22 @@
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
 
+variable "availabilityZone" {
+  type = string
+}
+
+variable "size" {
+  type = number
+}
+
+variable "encryption" {
+  type    = bool
+  default = false
+}
+
+variable "volumneType" {
+  type = string
+  default = "gp3"
+}
